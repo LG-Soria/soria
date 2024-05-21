e00a28f4b73921be0ac8248157826870c95d6ea9
LG-Soria <lucas_s96@outlook.com>
2024-05-21 20:34:08 -0300

    4to responsive commit

--- src/app/Navigation/Navigation.jsx
+++ src/app/Navigation/Navigation.jsx
@@ -13,13 +13,23 @@ function Navigation() {
         <div className='navigation'>
             <div className='logo'><p>LS</p></div>
             <div className="submenus">
-                <IoMenu  className={menu ? ('hidden'): ("burguerMenu")} onClick={openMenu}/>
+                <IoMenu className={menu ? ('hidden') : ("burguerMenu")} onClick={openMenu} />
                 <IoCloseSharp
-                onClick={openMenu}
-                className={menu ? ('closeMenu'): ("hidden")}
-                  />
+                    onClick={openMenu}
+                    className={menu ? ('closeMenu') : ("hidden")}
+                />
+            </div>
+            <div className='responsiveMenu'>
+                <div className={menu ? ('yesMenu') : ("noMenu")}>
+                    <ul className='flex flex-row gap-20 {}'>
+                        <li><a href="#heading">Inicio</a></li>
+                        <li><a href="#project">Portafolio</a></li>
+                        <li><a href="#contact">Contacto</a></li>
+                    </ul>
                 </div>
-            <div className={menu ? ('menu'): ("noMenu overflow-hidden") }>       
+
+            </div>
+            <div className='menu'>
                 <ul className='flex flex-row gap-20 {}'>
                     <li><a href="#heading">Inicio</a></li>
                     <li><a href="#project">Portafolio</a></li>
