999beff0a8f91df5dc357e69069da7da5031cfe9
LG-Soria <lucas_s96@outlook.com>
2024-05-22 18:28:34 -0300

    menu click funtion

--- src/app/Navigation/Navigation.jsx
+++ src/app/Navigation/Navigation.jsx
@@ -1,14 +1,27 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { IoMenu } from "react-icons/io5";
 import { IoCloseSharp } from "react-icons/io5";
 
 function Navigation() {
-    const [menu, set] = useState(false)
+    const [menu, setMenu] = useState(false)
+    const menuRef = useRef(null);
 
     const openMenu = () => {
-        set(state => !state)
-    }
+        setMenu(state => !state)
+    };
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+          setMenu(false);
+        }
+      };
+    
+      useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, []);
     return (
         <div className='navigation'>
             <div className='logo'><p>LS</p></div>
@@ -19,9 +32,9 @@ function Navigation() {
                     className={menu ? ('closeMenu') : ("hidden")}
                 />
             </div>
-            <div className='responsiveMenu'>
+            <div className='responsiveMenu'  ref={menuRef}>
                 <div className={menu ? ('yesMenu') : ("noMenu")}>
-                    <ul className='flex flex-row gap-20 {}'>
+                    <ul className='flex flex-row  '>
                         <li><a href="#heading">Inicio</a></li>
                         <li><a href="#project">Portafolio</a></li>
                         <li><a href="#contact">Contacto</a></li>
