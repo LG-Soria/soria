3fcf64c3ab4bca97442ffbc2c3e703cbbf635c56
LG-Soria <lucas_s96@outlook.com>
2024-05-27 00:19:23 -0300

    2do commit

--- src/app/Contact/Contacto.jsx
+++ src/app/Contact/Contacto.jsx
@@ -44,10 +44,10 @@ function Contacto(props) {
                         target='/blank' className='outlook'>lucas_s96@outlook.com</a>  o completando el siguiente formulario con tus datos y me pondré en contacto.</p></div>) : (<div className='contacto__texto'>  <h4>
                             DO YOU WANT TO GET IN TOUCH WITH ME?</h4>
                             <p>
-                                You can reach out through my social media channels. I'll be available to respond on both <a href='https://www.linkedin.com/in/lucas-soria-g/' target='/blank' rel="noreferrer" className='linkedin'>Linkedin</a>
+                                You can reach out through my social media channels. I&apos;ll be available to respond on both <a href='https://www.linkedin.com/in/lucas-soria-g/' target='/blank' rel="noreferrer" className='linkedin'>Linkedin</a> 
                                 and <a href='https://github.com/LG-Soria' target='/blank' rel="noreferrer" className='github'>GitHub</a>.
                                 You can also contact me by sending an email to <a href='mailto:lucas_s96@outlook.com'
-                                    target='/blank' className='outlook'>lucas_s96@outlook.com</a> or by filling out the following form with your information, and I'll get back to you.</p></div>)
+                                    target='/blank' className='outlook'>lucas_s96@outlook.com</a> or by filling out the following form with your information, and I&apos;ll get back to you.</p></div>)
             }
             <div className='contacto__formulario'>
                 <div>
